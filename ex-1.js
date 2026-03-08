337a4b4bab265b18b8fe49f2afc4881da35e5058
Tle <title1337@gmail.com>
2026-03-08 09:35:10 +0700

    fix: EX1

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,11 @@
-let isOver18;
-let hasCriminalBlacklist;
+//let isOver18;
+//let hasCriminalBlacklist;
 
-let isAllow;
+//let isAllow;
 
 // Start coding here
+let isOver18 = false;
+let hasCriminalBlacklist = false;
+let isAllow = isOver18 && !hasCriminalBlacklist;
 
 console.log(isAllow);
