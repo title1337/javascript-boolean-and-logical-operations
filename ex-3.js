32edc1cd73e7c826b918df71fb9ce0515e074280
Tle <title1337@gmail.com>
2026-03-08 09:48:48 +0700

    feat: EX3

--- ex-3.js
+++ ex-3.js
@@ -1,9 +1,10 @@
 //Exercise #3: Debugging (Boolean & Logical Operations)
 
 //Start coding here
-const numberA = 100;
-const numberB = "20";
+let numberA = 100;
+let numberB = "20";
 
+numberB = Number(numberB);
 numberA = 10;
 
 console.log(`Sum: ${numberA + numberB}`);
