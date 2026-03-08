d1e52d16fbe0c679b89d3162cf0d457d19fccdf2
Tle <title1337@gmail.com>
2026-03-08 09:44:44 +0700

    feat: EX2

--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,19 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000;
-let isWeekday;
-let hasBoughtProductFromITCategory;
-let hasAttendedDiscountEvent;
-let isPlatinum;
+let memberLevel = "Gold";
+let lastMonthSpent = 4000;
 
-let hasPromotion;
+
+let lastMonthPaidMoreThan4000 = lastMonthSpent > 4000;
+let isWeekday= true;
+let hasBoughtProductFromITCategory = false;
+let hasAttendedDiscountEvent = true;
+let isPlatinum = memberLevel == "Platinum";
+
+let hasPromotion = (lastMonthPaidMoreThan4000 && isWeekday 
+    && hasBoughtProductFromITCategory
+    && !hasAttendedDiscountEvent
+    && !isPlatinum);
 
 console.log(hasPromotion);
